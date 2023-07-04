78915f4914e5a4d60f7f3b10c1aa0eaec6ac6bf7
AzazaPG <133438776+AzazaPG@users.noreply.github.com>
2023-07-05 01:06:05 +0300

    Update task1.rs

--- hw2/task/src/task1.rs
+++ hw2/task/src/task1.rs
@@ -11,19 +11,21 @@ const QUOTE: &str = "Every face, every shop, bedroom window, public-house, and
 dark square is a picture feverishly turned--in search of what?
 It is the same with books. What do we seek through millions of pages?";
 
-
 fn main() {
-    find_term(SEARCH_TERM, QUOTE);
+    let answer = find_term(SEARCH_TERM, QUOTE);
+    println!("{}", answer);
 }
 
 fn find_term(search_term: &str, quote: &str) -> String {
     let lines: Vec<&str> = quote.split('\n').collect();
+
     for (index, line) in lines.iter().enumerate() {
         if line.contains(search_term) {
-            return format!("{}| {}", index + 1, line.trim());
+            return format!("{}: {}", index + 1, line.trim());
         }
     }
-    String::from("Не найдено нужное слово")
+
+    String::from("Word not found")
 }
 
 
